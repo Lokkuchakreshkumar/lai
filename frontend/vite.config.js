fe54ac37b758d82360a0981f1a561ddeec48b138
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-07-26 00:11:24 +0530

    removed popup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,30 +4,7 @@ import { resolve } from 'path';
 import { copyFileSync } from 'fs';
 import tailwindcss from '@tailwindcss/vite'
 
-// Custom plugin to copy manifest
-function copyManifest() {
-  return {
-    name: 'copy-manifest',
-    closeBundle() {
-      copyFileSync(
-        resolve(__dirname, 'manifest.json'),
-        resolve(__dirname, 'dist', 'manifest.json')
-      );
-    }
-  };
-}
 
 export default defineConfig({
   plugins: [react(), copyManifest(),tailwindcss()],
-  build:{
-    rollupOptions:{
-      input:{
-        popup:resolve(__dirname,'index.html'),
-        content:resolve(__dirname,'content.js')
-      },
-       output: {
-        entryFileNames: '[name].js' // 👈 Prevents hashing
-      }
-    }
-  }
 });
